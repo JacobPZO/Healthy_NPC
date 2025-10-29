a982068c957e38380439742633aea0a65d2cb18f
JacobPZO <156956701+JacobPZO@users.noreply.github.com>
2025-10-29 11:55:16 -0700

    project

---
+++ SRPNPC Project/Assets/Scripts/NPC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 4f;
+    [SerializeField] private float turnSpeed = 90f;
+
+
+
+    private void Start()
+    {
+
+    }
+
+    internal void TakeDamage(int amount)
+    {
+        GetComponent<IHealth>().TakeDamage(amount);
+    }
+
+    private void Update()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        transform.Rotate(0f, turnSpeed * Time.deltaTime, 0f);
+    }
+
+}
---
+++ SRPNPC Project/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,232 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void HPBar::Start()
+extern void HPBar_Start_m7691138B547AEE0BF85968A9A0537289BE8971DB (void);
+// 0x00000002 System.Void HPBar::HandleHPPctChanged(System.Single)
+extern void HPBar_HandleHPPctChanged_m968245FED4D0E814B2B41CAAFBBD11A9F91BA7EF (void);
+// 0x00000003 System.Void HPBar::.ctor()
+extern void HPBar__ctor_m68F8AA9018461B0AAB94A51D91E4CE492244EC5C (void);
+// 0x00000004 System.Void IHealth::add_OnHPPctChanged(System.Action`1<System.Single>)
+// 0x00000005 System.Void IHealth::remove_OnHPPctChanged(System.Action`1<System.Single>)
+// 0x00000006 System.Void IHealth::add_OnDied(System.Action)
+// 0x00000007 System.Void IHealth::remove_OnDied(System.Action)
+// 0x00000008 System.Void IHealth::TakeDamage(System.Int32)
+// 0x00000009 System.Void NPC::Start()
+extern void NPC_Start_m481C30C6C9B0824C7001A15907D82D7374CF31A0 (void);
+// 0x0000000A System.Void NPC::TakeDamage(System.Int32)
+extern void NPC_TakeDamage_m720A78A1784069E71EB8CB5FE474EFB90EB6EC08 (void);
+// 0x0000000B System.Void NPC::Update()
+extern void NPC_Update_m87D1148208FCB1196E8225846A297C23144D3628 (void);
+// 0x0000000C System.Void NPC::.ctor()
+extern void NPC__ctor_m06FE71FC7FFBCD78964792E60FF51E0C087CB9EE (void);
+// 0x0000000D System.Void NPCParticles::Start()
+extern void NPCParticles_Start_m7E1FD74592077C8E6969ED40F9E56F8C89196AB9 (void);
+// 0x0000000E System.Void NPCParticles::HandleNPCDied()
+extern void NPCParticles_HandleNPCDied_mA58B31051B29705E5A536ADF7D66D5C4FCF88764 (void);
+// 0x0000000F System.Void NPCParticles::.ctor()
+extern void NPCParticles__ctor_m4387C72B00A1A5AACE4E14AC42503D87A4D851E0 (void);
+// 0x00000010 System.Void NumberOfHitsHealth::add_OnHPPctChanged(System.Action`1<System.Single>)
+extern void NumberOfHitsHealth_add_OnHPPctChanged_mDB5C27A297DA88BE0FB17F5B4229527DB9229C1B (void);
+// 0x00000011 System.Void NumberOfHitsHealth::remove_OnHPPctChanged(System.Action`1<System.Single>)
+extern void NumberOfHitsHealth_remove_OnHPPctChanged_mB50551815E8E91E8C70E63C4A5DB150C70B82333 (void);
+// 0x00000012 System.Void NumberOfHitsHealth::add_OnDied(System.Action)
+extern void NumberOfHitsHealth_add_OnDied_mF2EFBD073AAA53A00887A9F8E78B73140261AC21 (void);
+// 0x00000013 System.Void NumberOfHitsHealth::remove_OnDied(System.Action)
+extern void NumberOfHitsHealth_remove_OnDied_m6F999F996E39E9E3B9461671CF10971CC0D5C71A (void);
+// 0x00000014 System.Single NumberOfHitsHealth::get_CurrentHpPct()
+extern void NumberOfHitsHealth_get_CurrentHpPct_m1FB13D36520D44185128E039DF0DDDB3B4B02137 (void);
+// 0x00000015 System.Void NumberOfHitsHealth::Start()
+extern void NumberOfHitsHealth_Start_mB5D4CABD57566210A90C0D17C0657125B587C134 (void);
+// 0x00000016 System.Void NumberOfHitsHealth::TakeDamage(System.Int32)
+extern void NumberOfHitsHealth_TakeDamage_m184636C31AE7FA9F9DDB4B5EFC86D9B082F9748A (void);
+// 0x00000017 System.Void NumberOfHitsHealth::Update()
+extern void NumberOfHitsHealth_Update_mCE34BFDAB9E58F7594F559A9455F72D2271AC403 (void);
+// 0x00000018 System.Collections.IEnumerator NumberOfHitsHealth::InvunlerabilityTimer()
+extern void NumberOfHitsHealth_InvunlerabilityTimer_m7C6C2DC467962F3A299A74A171CE9EFA5F9C0D2B (void);
+// 0x00000019 System.Void NumberOfHitsHealth::.ctor()
+extern void NumberOfHitsHealth__ctor_mCD26CA3CD6840FC0A7D03FAFB9077F2DDEC67876 (void);
+// 0x0000001A System.Void NumberOfHitsHealth/<InvunlerabilityTimer>d__15::.ctor(System.Int32)
+extern void U3CInvunlerabilityTimerU3Ed__15__ctor_m822913909B874B69231B4430ABF6B27021719B3F (void);
+// 0x0000001B System.Void NumberOfHitsHealth/<InvunlerabilityTimer>d__15::System.IDisposable.Dispose()
+extern void U3CInvunlerabilityTimerU3Ed__15_System_IDisposable_Dispose_m2F5DA22B3F93692905E3311C33DD433D652EA6F2 (void);
+// 0x0000001C System.Boolean NumberOfHitsHealth/<InvunlerabilityTimer>d__15::MoveNext()
+extern void U3CInvunlerabilityTimerU3Ed__15_MoveNext_m26CDDCDD0E0FC857053C7ABF4492900AD746AEEC (void);
+// 0x0000001D System.Object NumberOfHitsHealth/<InvunlerabilityTimer>d__15::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CInvunlerabilityTimerU3Ed__15_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mB44FE4A3001084750017929EB99868D0936C92E7 (void);
+// 0x0000001E System.Void NumberOfHitsHealth/<InvunlerabilityTimer>d__15::System.Collections.IEnumerator.Reset()
+extern void U3CInvunlerabilityTimerU3Ed__15_System_Collections_IEnumerator_Reset_m17FD5A1D2FFAF4CBE4540ABA064F700CC74D6800 (void);
+// 0x0000001F System.Object NumberOfHitsHealth/<InvunlerabilityTimer>d__15::System.Collections.IEnumerator.get_Current()
+extern void U3CInvunlerabilityTimerU3Ed__15_System_Collections_IEnumerator_get_Current_mACA3C897FE596B4FFCD567F14864EEC0D681CACA (void);
+// 0x00000020 System.Void NumberOfHitsHealth/<>c::.cctor()
+extern void U3CU3Ec__cctor_m002487970D704D0661C410043577CE419F566A20 (void);
+// 0x00000021 System.Void NumberOfHitsHealth/<>c::.ctor()
+extern void U3CU3Ec__ctor_m81B77B18ADDDB2BC43D9FBCE83733C545EFA9946 (void);
+// 0x00000022 System.Void NumberOfHitsHealth/<>c::<.ctor>b__16_0(System.Single)
+extern void U3CU3Ec_U3C_ctorU3Eb__16_0_m118437E453F9A5B21FB3C927FFE6ADA51C4E22F1 (void);
+// 0x00000023 System.Void NumberOfHitsHealth/<>c::<.ctor>b__16_1()
+extern void U3CU3Ec_U3C_ctorU3Eb__16_1_mD694A89CE54452DA9F57C34370C4ABD6D494541E (void);
+// 0x00000024 System.Void StandardHealth::add_OnHPPctChanged(System.Action`1<System.Single>)
+extern void StandardHealth_add_OnHPPctChanged_m1AC787AE9B7E91AF902887FB14D880429DAC9390 (void);
+// 0x00000025 System.Void StandardHealth::remove_OnHPPctChanged(System.Action`1<System.Single>)
+extern void StandardHealth_remove_OnHPPctChanged_mCEABC68EF28996AF0DC5BA9F64EE1AC8C95BF7B5 (void);
+// 0x00000026 System.Void StandardHealth::add_OnDied(System.Action)
+extern void StandardHealth_add_OnDied_mA8874C741B91F1FF9BDAAC8516FC65A5EAECCFF9 (void);
+// 0x00000027 System.Void StandardHealth::remove_OnDied(System.Action)
+extern void StandardHealth_remove_OnDied_m28CE6009422972E02C1BC6EAB1289BB6ADA97BFA (void);
+// 0x00000028 System.Void StandardHealth::Start()
+extern void StandardHealth_Start_mC3370C56A78ADFAEC5D193B889ED904123544AB1 (void);
+// 0x00000029 System.Single StandardHealth::get_CurrentHpPct()
+extern void StandardHealth_get_CurrentHpPct_m66113D1172CB1669C5210F5D8355C6B7D1C5C288 (void);
+// 0x0000002A System.Void StandardHealth::TakeDamage(System.Int32)
+extern void StandardHealth_TakeDamage_m72012C2C23E5AC3CA33036DE2163DDF35AEA96F2 (void);
+// 0x0000002B System.Void StandardHealth::Die()
+extern void StandardHealth_Die_mBCA674BD4572CC7F23BC116EA0DF4947D9C15873 (void);
+// 0x0000002C System.Void StandardHealth::Update()
+extern void StandardHealth_Update_mF64F2CF025C88D1AA78B702553E508E4B77B8EF2 (void);
+// 0x0000002D System.Void StandardHealth::.ctor()
+extern void StandardHealth__ctor_m4A52DF04304CAB2EA92AAC850A5E1ECE5005336D (void);
+// 0x0000002E System.Void StandardHealth/<>c::.cctor()
+extern void U3CU3Ec__cctor_m5DC1E6E6FF06CB94C9836871728C696D708EC2B8 (void);
+// 0x0000002F System.Void StandardHealth/<>c::.ctor()
+extern void U3CU3Ec__ctor_m6B8B7275C4C2740B4182F03EDB70486B7F103188 (void);
+// 0x00000030 System.Void StandardHealth/<>c::<.ctor>b__14_0(System.Single)
+extern void U3CU3Ec_U3C_ctorU3Eb__14_0_mC30A2A776E5229F409ED602563D6CAABEF27503D (void);
+// 0x00000031 System.Void StandardHealth/<>c::<.ctor>b__14_1()
+extern void U3CU3Ec_U3C_ctorU3Eb__14_1_mEFBC00986A4EA01F325D8CFBE48920B2D7E96C9C (void);
+static Il2CppMethodPointer s_methodPointers[49] = 
+{
+	HPBar_Start_m7691138B547AEE0BF85968A9A0537289BE8971DB,
+	HPBar_HandleHPPctChanged_m968245FED4D0E814B2B41CAAFBBD11A9F91BA7EF,
+	HPBar__ctor_m68F8AA9018461B0AAB94A51D91E4CE492244EC5C,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NPC_Start_m481C30C6C9B0824C7001A15907D82D7374CF31A0,
+	NPC_TakeDamage_m720A78A1784069E71EB8CB5FE474EFB90EB6EC08,
+	NPC_Update_m87D1148208FCB1196E8225846A297C23144D3628,
+	NPC__ctor_m06FE71FC7FFBCD78964792E60FF51E0C087CB9EE,
+	NPCParticles_Start_m7E1FD74592077C8E6969ED40F9E56F8C89196AB9,
+	NPCParticles_HandleNPCDied_mA58B31051B29705E5A536ADF7D66D5C4FCF88764,
+	NPCParticles__ctor_m4387C72B00A1A5AACE4E14AC42503D87A4D851E0,
+	NumberOfHitsHealth_add_OnHPPctChanged_mDB5C27A297DA88BE0FB17F5B4229527DB9229C1B,
+	NumberOfHitsHealth_remove_OnHPPctChanged_mB50551815E8E91E8C70E63C4A5DB150C70B82333,
+	NumberOfHitsHealth_add_OnDied_mF2EFBD073AAA53A00887A9F8E78B73140261AC21,
+	NumberOfHitsHealth_remove_OnDied_m6F999F996E39E9E3B9461671CF10971CC0D5C71A,
+	NumberOfHitsHealth_get_CurrentHpPct_m1FB13D36520D44185128E039DF0DDDB3B4B02137,
+	NumberOfHitsHealth_Start_mB5D4CABD57566210A90C0D17C0657125B587C134,
+	NumberOfHitsHealth_TakeDamage_m184636C31AE7FA9F9DDB4B5EFC86D9B082F9748A,
+	NumberOfHitsHealth_Update_mCE34BFDAB9E58F7594F559A9455F72D2271AC403,
+	NumberOfHitsHealth_InvunlerabilityTimer_m7C6C2DC467962F3A299A74A171CE9EFA5F9C0D2B,
+	NumberOfHitsHealth__ctor_mCD26CA3CD6840FC0A7D03FAFB9077F2DDEC67876,
+	U3CInvunlerabilityTimerU3Ed__15__ctor_m822913909B874B69231B4430ABF6B27021719B3F,
+	U3CInvunlerabilityTimerU3Ed__15_System_IDisposable_Dispose_m2F5DA22B3F93692905E3311C33DD433D652EA6F2,
+	U3CInvunlerabilityTimerU3Ed__15_MoveNext_m26CDDCDD0E0FC857053C7ABF4492900AD746AEEC,
+	U3CInvunlerabilityTimerU3Ed__15_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mB44FE4A3001084750017929EB99868D0936C92E7,
+	U3CInvunlerabilityTimerU3Ed__15_System_Collections_IEnumerator_Reset_m17FD5A1D2FFAF4CBE4540ABA064F700CC74D6800,
+	U3CInvunlerabilityTimerU3Ed__15_System_Collections_IEnumerator_get_Current_mACA3C897FE596B4FFCD567F14864EEC0D681CACA,
+	U3CU3Ec__cctor_m002487970D704D0661C410043577CE419F566A20,
+	U3CU3Ec__ctor_m81B77B18ADDDB2BC43D9FBCE83733C545EFA9946,
+	U3CU3Ec_U3C_ctorU3Eb__16_0_m118437E453F9A5B21FB3C927FFE6ADA51C4E22F1,
+	U3CU3Ec_U3C_ctorU3Eb__16_1_mD694A89CE54452DA9F57C34370C4ABD6D494541E,
+	StandardHealth_add_OnHPPctChanged_m1AC787AE9B7E91AF902887FB14D880429DAC9390,
+	StandardHealth_remove_OnHPPctChanged_mCEABC68EF28996AF0DC5BA9F64EE1AC8C95BF7B5,
+	StandardHealth_add_OnDied_mA8874C741B91F1FF9BDAAC8516FC65A5EAECCFF9,
+	StandardHealth_remove_OnDied_m28CE6009422972E02C1BC6EAB1289BB6ADA97BFA,
+	StandardHealth_Start_mC3370C56A78ADFAEC5D193B889ED904123544AB1,
+	StandardHealth_get_CurrentHpPct_m66113D1172CB1669C5210F5D8355C6B7D1C5C288,
+	StandardHealth_TakeDamage_m72012C2C23E5AC3CA33036DE2163DDF35AEA96F2,
+	StandardHealth_Die_mBCA674BD4572CC7F23BC116EA0DF4947D9C15873,
+	StandardHealth_Update_mF64F2CF025C88D1AA78B702553E508E4B77B8EF2,
+	StandardHealth__ctor_m4A52DF04304CAB2EA92AAC850A5E1ECE5005336D,
+	U3CU3Ec__cctor_m5DC1E6E6FF06CB94C9836871728C696D708EC2B8,
+	U3CU3Ec__ctor_m6B8B7275C4C2740B4182F03EDB70486B7F103188,
+	U3CU3Ec_U3C_ctorU3Eb__14_0_mC30A2A776E5229F409ED602563D6CAABEF27503D,
+	U3CU3Ec_U3C_ctorU3Eb__14_1_mEFBC00986A4EA01F325D8CFBE48920B2D7E96C9C,
+};
+static const int32_t s_InvokerIndices[49] = 
+{
+	3310,
+	2716,
+	3310,
+	0,
+	0,
+	0,
+	0,
+	0,
+	3310,
+	2668,
+	3310,
+	3310,
+	3310,
+	3310,
+	3310,
+	2685,
+	2685,
+	2685,
+	2685,
+	3262,
+	3310,
+	2668,
+	3310,
+	3226,
+	3310,
+	2668,
+	3310,
+	3170,
+	3226,
+	3310,
+	3226,
+	5007,
+	3310,
+	2716,
+	3310,
+	2685,
+	2685,
+	2685,
+	2685,
+	3310,
+	3262,
+	2668,
+	3310,
+	3310,
+	3310,
+	5007,
+	3310,
+	2716,
+	3310,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	49,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
